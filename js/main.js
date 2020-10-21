7519e30a8b7928d562a806cd70b66fffac13a28d
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-10-20 21:15:28 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
 
 var config = {
-    matrixServerUrl: 'https://matrix.safesupport.chat',
-    botId: '@help-bot:safesupport.chat',
-    roomName: 'Support Chat',
-    termsUrl: 'https://tosdr.org/',
-    introMessage: "This is just a demo! There is probably no-one at the other end. You should probaby email us instead. But if you're lucky, there might be someone online to say hi!",
-    agreementMessage: 'Do you want to continue?',
-    confirmationMessage: 'Waiting for a facilitator to join the chat...',
-    exitMessage: 'The chat is closed. You may close this window.',
-    chatUnavailableMessage: 'The chat service is not available right now. Please try again later.',
-    anonymousDisplayName: 'Anonymous',
-    position: 'bottom right',
-    size: 'large',
+    settingsEndpoint: 'https://safesupport-admin.herokuapp.com/api/get-settings'
 }
 
 EmbeddableChatbox.mount(config);
