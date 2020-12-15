2f89f676d3a7483b9aefe0f26a6975c3717307fc
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-12-15 10:26:51 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 var config = {
-    settingsEndpoint: 'https://safesupport-admin.herokuapp.com/api/get-settings',
-    matrixServerUrl: 'https://matrix.safesupport.chat',
+    matrixServerUrl: 'https://matrix.safesupport.chat'
 }
 
 EmbeddableChatbox.mount(config);
